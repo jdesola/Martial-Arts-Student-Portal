bd8ce02d4092eb188a2d6ab88ec7c8f9561c4118
Josh <joshdesola@gmail.com>
2021-01-19 22:06:24 -0500

    initializing student-portal project, added Vue, began nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <nav-bar></nav-bar>
+    <router-view/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import navBar from './components/nav-bar.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { navBar },
+  
 }
 </script>
 
 <style>
-#app {
+v-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card
+    height="400"
+    width="256"
+
+  >
+    <v-navigation-drawer >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Slomovitz Martial Arts Academy
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Student Portal
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+    name: "navBar",
+    data () {
+      return {
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
+  
+}
+</script>
+
+<style>
+
+</style>
