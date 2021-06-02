c6cdfc0fb7d17638b7ab424deb768ebb0ac273ac
Josh DeSola <joshdesola@gmail.com>
2021-06-02 17:09:23 -0400

    refined classes to be more specific and follow OOP guidelines better

--- checkInAndPortalTest/checkInAndPortalTest/src/main/java/com/desola/checkInAndPortalTest/DAO/FamilyDAO.java
+++ checkInAndPortalTest/checkInAndPortalTest/src/main/java/com/desola/checkInAndPortalTest/DAO/FamilyDAO.java
@@ -6,7 +6,7 @@ import com.desola.checkInAndPortalTest.Model.Family;
 
 public interface FamilyDAO {
 		public long addFamily(Family family);
-		public void markFamilyInactive(long familyId);
+		public void toggleFamilyActiveStatus(Family family);
 		public void updateFamily(Family family);
 		public Family getFamily(long familyId);
 		public List<Family> getAllFamilies();
--- checkInAndPortalTest/checkInAndPortalTest/src/main/java/com/desola/checkInAndPortalTest/Model/Family.java
+++ checkInAndPortalTest/checkInAndPortalTest/src/main/java/com/desola/checkInAndPortalTest/Model/Family.java
@@ -1,14 +1,10 @@
 package com.desola.checkInAndPortalTest.Model;
 
-import java.util.List;
 
 public class Family {
 	private long familyId;
 	private String familyName;
 	private Boolean active;
-	private List<Address> addresses;
-	private List<PhoneNumber> phoneNumbers;
-	private List<EmailAddress> emailAddresses;
 	
 	public long getFamilyId() {
 		return familyId;
@@ -27,24 +23,6 @@ public class Family {
 	}
 	public void setActive(Boolean active) {
 		this.active = active;
-	}
-	public List<Address> getAddresses() {
-		return addresses;
-	}
-	public void setAddresses(List<Address> addresses) {
-		this.addresses = addresses;
-	}
-	public List<PhoneNumber> getPhoneNumbers() {
-		return phoneNumbers;
-	}
-	public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
-		this.phoneNumbers = phoneNumbers;
-	}
-	public List<EmailAddress> getEmailAddresses() {
-		return emailAddresses;
-	}
-	public void setEmailAddresses(List<EmailAddress> emailAddresses) {
-		this.emailAddresses = emailAddresses;
 	}
 
 	
